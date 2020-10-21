9253797db53f1a97d8533bec5a02942a482f1cd5
moran666666 <396124008@qq.com>
2020-10-21 09:14:23 +0800

    commit1

--- extern/sector-storage/stores/local.go
+++ extern/sector-storage/stores/local.go
@@ -340,7 +340,7 @@ func (st *Local) AcquireSector(ctx context.Context, sid abi.SectorID, spt abi.Re
 			continue
 		}
 
-		si, err := st.index.StorageFindSector(ctx, sid, fileType, spt, false)
+		si, err := st.CheckDeclareSector(ctx, sid, fileType, spt, pathType)
 		if err != nil {
 			log.Warnf("finding existing sector %d(t:%d) failed: %+v", sid, fileType, err)
 			continue
@@ -595,3 +595,27 @@ func (st *Local) FsStat(ctx context.Context, id ID) (fsutil.FsStat, error) {
 }
 
 var _ Store = &Local{}
+
+func (st *Local) CheckDeclareSector(ctx context.Context, sid abi.SectorID, fileType SectorFileType, spt abi.RegisteredSealProof, pathType PathType) ([]SectorStorageInfo, error) {
+	si0, err := st.index.StorageFindSector(ctx, sid, fileType, spt, false)
+	if len(si0) > 0 || err != nil {
+		return si0, err
+	}
+	if pathType == PathStorage {
+		for id, path := range st.paths {
+			if sstInfo, err := st.index.StorageInfo(ctx, id); err == nil {
+				if sstInfo.CanStore {
+					p := filepath.Join(path.local, fileType.String(), SectorName(sid))
+					_, err := os.Stat(p)
+					if os.IsNotExist(err) || err != nil {
+						continue
+					}
+					if err := st.index.StorageDeclareSector(ctx, id, sid, fileType, sstInfo.CanStore); err != nil {
+						continue
+					}
+				}
+			}
+		}
+	}
+	return st.index.StorageFindSector(ctx, sid, fileType, spt, false)
+}
